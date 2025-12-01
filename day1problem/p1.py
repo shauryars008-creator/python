9b52314b824bd1cfc10d9efc3247c3b952b91356
shauryars008-creator <shauryars008@gmail.com>
2025-12-01 23:19:23 +0530

    updated

---
+++ day1problem/p1.py
@@ -0,0 +1,13 @@
+x=input("enter first number = ")
+x=float(x)
+y=input("enter second number = ")
+y=float(y)
+opr=input("enter the operation +,-,* or / = ")
+if opr == "+" :
+    print("sum =",x+y)
+elif opr == "-" :
+    print("difference =" ,x-y)
+elif opr == "*" :
+    print("product =" ,x*y)
+elif opr == "/" :
+    print("quotient =" ,x/y)
