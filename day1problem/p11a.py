937a9f1c9a15707a144b6e421a3a2d66dbba111a
shauryars008-creator <shauryars008@gmail.com>
2025-12-02 15:16:33 +0530

    updated

--- day1problem/p11a.py
+++ day1problem/p11a.py
@@ -1,1 +1,19 @@
-ECHO is on.
+import random
+score = 0
+for i in range(10) :
+    print ( "chance" ,i+1) 
+    a = input("Enter a number between 1 to 100 : ")
+    a = int(a)
+    b = random.randint(1,100)
+    print("your number : " , a ,"code's number : " , b)
+    if a > b:
+        score +=1
+    elif a < b:
+        score -=1
+    elif a == b:
+        score +=3
+print("score : " ,score)
+if score > 0 :
+    print("YOU WON")
+elif score < 0 :
+    print("CODE WON")
