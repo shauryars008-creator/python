19e599cf733f490c96d587d1e4b8166d5cd19fa1
shauryars008-creator <shauryars008@gmail.com>
2025-12-02 16:26:30 +0530

    updated

--- day1problem/p10.py
+++ day1problem/p10.py
@@ -2,10 +2,10 @@ pow = input("enter the power 2,3 or 4 : ")
 pow = int(pow)
 if pow == 2 :
     print("to prove (a+b)**2 = a**2 + b**2 + 2*a*b")
-    a=input("enter first number = ")
-    a=float(a)
-    b=input("enter second number = ")
-    b=float(b)
+    a = input("enter first number = ")
+    a = float(a)
+    b = input("enter second number = ")
+    b = float(b)
     lhs = (a+b)**2
     rhs = a**2 + b**2 + 2*a*b
     print("lhs =" , lhs,"rhs =" , rhs)
@@ -13,10 +13,10 @@ if pow == 2 :
         print ("lhs == rhs HENCE PROVED")
 if pow ==3 :
     print("to prove (a+b)**3 = a**3 + b**3 + 3*a**2*b +3*b**2*a")
-    a=input("enter first number = ")
-    a=float(a)
-    b=input("enter second number = ")
-    b=float(b)
+    a = input("enter first number = ")
+    a = float(a)
+    b = input("enter second number = ")
+    b = float(b)
     lhs = (a+b)**3
     rhs = a**3 + b**3 + 3*a**2*b +3*b**2*a
     print("lhs =" , lhs,"rhs =" , rhs)
@@ -24,10 +24,10 @@ if pow ==3 :
         print ("lhs == rhs HENCE PROVED")
 if pow == 4:
     print("to prove (a+b)**4 = a**4 + b**4 + 4*a**3*b + 4*b**3*a + 6*a**2*b**2")
-    a=input("enter first number = ")
-    a=float(a)
-    b=input("enter second number = ")
-    b=float(b)
+    a = input("enter first number = ")
+    a = float(a)
+    b = input("enter second number = ")
+    b = float(b)
     lhs = (a+b)**4
     rhs = a**4 + b**4 + 4*a**3*b + 4*b**3*a + 6*a**2*b**2
     print("lhs =" , lhs,"rhs =" , rhs)
