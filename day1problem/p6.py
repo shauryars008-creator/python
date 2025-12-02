98fbafce5c5e4f72616b64636c355466a7b10fcc
shauryars008-creator <shauryars008@gmail.com>
2025-12-02 18:42:37 +0530

    updated

--- day1problem/p6.py
+++ day1problem/p6.py
@@ -1,2 +1,8 @@
-
+a = input("enter the character : ")
+num = input("enter the number of times : ")
+num = int(num)
+for i in range(num) :
+    for j in range(i+1) :
+        print( a,end = "")
+    print('\n')
     
