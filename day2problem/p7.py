d4eb789bbc8a313beebb3b026544a80e11773693
shauryars008-creator <shauryars008@gmail.com>
2025-12-06 22:59:54 +0530

    updated

--- day2problem/p7.py
+++ day2problem/p7.py
@@ -1,1 +1,17 @@
-ECHO is on.
+strr = "Hello World and Hello Earth"
+lst = []
+lstcnt = []
+for e in strr.split() : 
+    for i in e :
+        if i in lst :
+            for j,k in enumerate(lst) :
+                if k == i :
+                    index = j
+            lstcnt[index] +=1
+        else :
+            lst.append(i)
+            lstcnt.append(1)
+for i in range(len(lst)) :
+    print(f"{lst[i]} = {lstcnt[i]}")
+        
+    
